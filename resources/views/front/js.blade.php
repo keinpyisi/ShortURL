b9649d8d7c48aa25de787d1e477e8b70cfc03441
Y Wyne Ko <59800470+ywyneko@users.noreply.github.com>
2022-07-07 18:40:03 +0630

    remove something

--- resources/views/front/js.blade.php
+++ resources/views/front/js.blade.php
@@ -1,11 +1,10 @@
-<script src="{{asset('/dist/js/jquery.min.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" fullBase="1" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="{{asset('/dist/js/bootstrap.min.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="{{asset('/dist/js/owl.carousel.min.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="{{asset('/dist/js/wow.min.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.4/dist/clipboard.min.js" fullBase="1" type="60e0e18519daa1680f4829fb-text/javascript"></script><script src="https://cdn.jsdelivr.net/gh/jnicol/particleground@eac0d29a85e12523de625845e2cd30be3fa266b6/jquery.particleground.min.js" fullBase="1" type="60e0e18519daa1680f4829fb-text/javascript"></script><script src="https://cdn.jsdelivr.net/npm/select2@4.0.11/dist/js/select2.min.js" fullBase="1" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="{{asset('/dist/js/front.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script src="{{asset('/dist/js/app.js')}}" type="60e0e18519daa1680f4829fb-text/javascript"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.4.0/clipboard.js"></script> --}}
